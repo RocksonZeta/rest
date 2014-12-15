995d5a47667555aaa19f958e11c2ff81fb402565
uzoice <uzoice@163.com>
2014-12-15 23:10:16 +0800

    Add Limit method

--- FormFile.go
+++ FormFile.go
@@ -2,6 +2,7 @@ package rest
 
 import (
 	"bufio"
+	"bytes"
 	"io"
 	"mime/multipart"
 	"os"
@@ -11,15 +12,22 @@ type FormFile struct {
 	FileName    string
 	ContentType string
 	File        multipart.File
+	limit       int64
 }
 
-func (this *FormFile) save(path string, limit string) int64 {
+func (this *FormFile) Limit(len int64) *FormFile {
+	this.limit = len
+	return this
+}
+
+func (this *FormFile) Save(path string, limit int) int64 {
 	defer this.File.Close()
 	of, e := os.OpenFile(path, os.O_WRONLY|os.O_CREATE, 0644)
 	if nil != e {
 		panic(e.Error())
 	}
 	defer of.Close()
+	buffer := bytes.n
 	bof := bufio.NewWriterSize(of, 8*1024)
 	writeLen, e := io.Copy(bof, this.File)
 	if nil != e {
